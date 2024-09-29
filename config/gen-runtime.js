dede45c32fb0d0bc4b0e18c6128b45ceec068dbc
caufande <3823642480@qq.com>
2024-09-29 17:48:20 +0800

    env: 运行时参数环境变量生成小工具

---
+++ config/gen-runtime.js
@@ -0,0 +1,8 @@
+import clipboard from 'clipboardy';
+import { getDirname } from 'esm-entry';
+import * as fsp from 'fs/promises';
+
+const runtimeConfigFile = getDirname(import.meta.url) + '/runtime.json';
+const runtimeConfig = (await fsp.readFile(runtimeConfigFile)).toString();
+const copyString = JSON.stringify(JSON.stringify(JSON.parse(runtimeConfig)));
+clipboard.write(copyString);
