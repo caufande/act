520ea7a2a1618bd9c265493d882f292d88ffc3b5
caufande <3823642480@qq.com>
2024-10-16 02:35:16 +0800

    add(db-operator-taro): 给 taro 操作器加入多语言选项

--- packages/db-operator-taro/lib/index.ts
+++ packages/db-operator-taro/lib/index.ts
@@ -5,10 +5,12 @@
 declare module '.';
 
 import IOperator from '@cauact/db/lib/Operator';
+import LangInfo from '@cauact/db/lib/Operator/LangInfo';
 import Requester from './Requester';
 import Storager from './Storager';
 
 export default class Operator extends IOperator {
+	langInfo = new LangInfo('zh-CN');
 	requesterIniter = Requester;
 	storagerIniter = Storager;
 }
