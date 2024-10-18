069899f8488ce5e32714ef8bcb27fa343a7aac1f
caufande <3823642480@qq.com>
2024-10-19 00:15:20 +0800

    refactor(db): 错误处理加上类型守卫

--- packages/db/lib/errors/index.ts
+++ packages/db/lib/errors/index.ts
@@ -4,12 +4,15 @@
  */
 declare module '.';
 
+import { Static, TObject, Type } from '@sinclair/typebox';
+import { Value } from '@sinclair/typebox/value';
 import { errorDefs } from './defs';
 import { getTip } from './names';
 
 export type ErrorType = keyof typeof errorDefs;
-export type InfosColl = { [I in ErrorType]: Parameters<(typeof errorDefs)[I]>[0]; };
+export type InfosColl = { [I in ErrorType]: Static<TObject<(typeof errorDefs)[I]>> };
 
 export function throwError<T extends ErrorType>(errorType: T, infos: InfosColl[T]): never {
+	Value.Assert(Type.Object(errorDefs[errorType]), infos);
 	throw [getTip(errorType), infos];
 }
