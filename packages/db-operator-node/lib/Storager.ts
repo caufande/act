14e5bda2a312ea56b9783903d485e01d92052db3
caufande <3823642480@qq.com>
2024-11-05 00:18:06 +0800

    add(db-operator-node): 简单实现一个进程内缓存器

--- packages/db-operator-node/lib/Storager.ts
+++ packages/db-operator-node/lib/Storager.ts
@@ -7,22 +7,25 @@ declare module './Storager';
 import { Storager as IStorager } from '@cauact/db';
 
 export default class Storager<T> extends IStorager<T> {
+	static readonly storages = new Map<string, any>();
 	protected async batchGetOrigin(keys: readonly string[]): Promise<unknown> {
-		return keys.map(_ => null);
+		return keys.map(k => storages.get(k) ?? null);
 	}
 	protected async getOrigin(key: string): Promise<unknown> {
-		return null;
+		return storages.get(key) ?? null;
 	}
 	async setOrigin(key: string, value: T): Promise<boolean> {
-		return false;
+		return storages.set(key, value), true;
 	}
 	async batchSetOrigin(kvs: Map<string, T>): Promise<boolean> {
-		return false;
+		return kvs.forEach((v, k) => storages.set(k, v)), true;
 	}
 	async clear(): Promise<void> {
-		1 + 1;
+		storages.clear();
 	}
 	async delete(key: string): Promise<void> {
-		1 + 1;
+		storages.delete(key);
 	}
 }
+
+const storages = Storager.storages;
